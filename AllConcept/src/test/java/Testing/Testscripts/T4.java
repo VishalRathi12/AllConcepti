feb1595c0059f009376f7ab848e24e412bc94753
PC <PC@192.168.0.116>
2020-10-05 23:09:58 +0530

    This is th second commit

--- AllConcept/src/test/java/Testing/Testscripts/T4.java
+++ AllConcept/src/test/java/Testing/Testscripts/T4.java
@@ -1,5 +1,39 @@
 package Testing.Testscripts;
 
-public class T4 {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class T4 {
+public static void main(String[] args) throws IOException {
+	
+
+
+	File f = new File("C:\\Users\\pc\\Documents\\VishalSeleniumCSV.csv");
+	FileReader fr = new FileReader(f);
+	BufferedReader b  = new BufferedReader(fr);
+	String s;
+	while((s=b.readLine())!=null)
+	{
+		System.out.println(s);
+		
+		String[] s1 = s.split(",");
+		
+		for(int i=0;i<s1.length;i++)
+		{
+			System.out.println(s1[i]);
+		}
+		
+	}
+	
+	
+	
+	
+	
+}
+	
+	
+	
 }
